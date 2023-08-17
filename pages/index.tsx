7006872999767aedfae3ee886e5e78481b1f210b
Jason <jason.aw@attspace.ventures>
2023-08-17 19:20:04 +0800

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Bridge from '../components/Icons/Bridge'
-import Logo from '../components/Icons/Logo'
 import Modal from '../components/Modal'
 import cloudinary from '../utils/cloudinary'
 import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
@@ -18,23 +17,84 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto()
 
   const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null)
+  const [uploadedResponses, setUploadedResponses] = useState<Response[]>([]);
 
-  useEffect(() => {
-    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
-    if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current.scrollIntoView({ block: 'center' })
-      setLastViewedPhoto(null)
+  
+  const uploadPhoto = async (file: File) => {
+      const apiUrl = 'https://api.cloudinary.com/v1_1/dxkvwzjs3/image/upload'; // Replace with your actual API endpoint URL
+      
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('upload_preset', process.env.CLOUDINARY_PRESET_KEY);
+      
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          body: formData,
+        });
+        console.log("form data", formData)
+        
+        if (!response.ok) {
+          alert('Failed to upload photo');
+        }
+        
+        const responseData = await response.json();
+        return responseData;
+      } catch (error) {
+        console.error('Error uploading photo: ' + error.message);
+      }
     }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
+    
+    const [uploading, setUploading] = useState(false);
+    const [selectedFile, setSelectedFile] = useState<File[]>([]);
+    
 
-  return (
-    <>
+    const handleFileChange = ( event) => {
+      setSelectedFile([...event.target.files]);
+    };
+    
+    const handleUploadClick = async () => {
+      if (selectedFile.length === 0) {
+        alert('Please select one or more files.');
+        return;
+      }
+      
+      try {
+        setUploading(true);
+        
+        const responses = await Promise.all(
+          selectedFile.map(async (file) => {
+            const response = await uploadPhoto(file);
+            return response;
+          })
+          );
+          
+          console.log('Photos uploaded successfully:', responses);
+          alert("Upload Successfully! Pls Refresh");
+          setUploadedResponses((prevResponses) => [...prevResponses, ...responses]);
+          setSelectedFile([]); 
+        } catch (error) {
+          console.error('Error uploading photos:', error);
+        } finally {
+          setUploading(false); 
+        }
+      };
+      
+      useEffect(() => {
+        if (lastViewedPhoto && !photoId) {
+          lastViewedPhotoRef.current.scrollIntoView({ block: 'center' })
+          setLastViewedPhoto(null)
+        }
+      }, [photoId, lastViewedPhoto, setLastViewedPhoto, uploadedResponses])
+
+      return (
+        <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>Phuket 2023 Photos</title>
         <meta
           property="og:image"
           content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
+          />
         <meta
           name="twitter:image"
           content="https://nextjsconf-pics.vercel.app/og-image.png"
@@ -50,29 +110,26 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           />
         )}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+          <div className="after:content relative mb-4 flex h-[260px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-10 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className="flex max-h-full max-w-full items-center justify-center">
                 <Bridge />
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
-            <Logo />
-            <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+            {/* <Logo /> */}
+            <h1 className="mt-2 pt- mb-4 text-base font-bold uppercase tracking-widest justify-end">
+              Phuket 2023 Photos
             </h1>
-            <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
-            </p>
-            <a
-              className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href="https://vercel.com/new/clone?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-cloudinary&project-name=nextjs-image-gallery&repository-name=with-cloudinary&env=NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET,CLOUDINARY_FOLDER&envDescription=API%20Keys%20from%20Cloudinary%20needed%20to%20run%20this%20application"
-              target="_blank"
-              rel="noreferrer"
+            <input className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4" type="file" multiple onChange={handleFileChange} />
+            <button
+              className="pointer z-10 mt-4 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
+              onClick={handleUploadClick}
+              disabled={uploading}
             >
-              Clone and Deploy
-            </a>
+              {uploading ? 'Uploading...' : 'Upload Photos'}
+            </button>
+
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
@@ -101,36 +158,6 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           ))}
         </div>
       </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
-        Thank you to{' '}
-        <a
-          href="https://edelsonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Josh Edelson
-        </a>
-        ,{' '}
-        <a
-          href="https://www.newrevmedia.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Jenny Morgan
-        </a>
-        , and{' '}
-        <a
-          href="https://www.garysextonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Gary Sexton
-        </a>{' '}
-        for the pictures.
-      </footer>
     </>
   )
 }
@@ -172,3 +199,5 @@ export async function getStaticProps() {
     },
   }
 }
+
+
