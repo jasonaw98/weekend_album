5a64c5fd90fae2da5388c671626529a0dcbe1a18
Jason <jason.aw@attspace.ventures>
2023-08-18 18:22:02 +0800

    settimeout

--- components/uploadButton.tsx
+++ components/uploadButton.tsx
@@ -2,15 +2,22 @@
 import { CldUploadButton } from 'next-cloudinary';
 import { useRouter } from 'next/navigation';
 
+export type UploadResult = {
+    info: {
+      public_id: string;
+    };
+    event: "success";
+  };
+
 const UploadButton = () => {
 
     const router = useRouter();
     return <>
     <CldUploadButton 
-    onUpload={() => {
+    onQueuesEnd={(result: UploadResult) => {
         setTimeout(() => {
             router.refresh();
-        }, 1000)
+        }, 2000)
     }}
     uploadPreset="u5cfnoki" 
     className="pointer z-10 mt-4 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"/>
